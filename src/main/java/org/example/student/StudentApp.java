5fa5666948bcb824f426282d21d95cf1c204b358
musobek <musobek253@gmail.com>
2023-11-29 20:51:48 +0500

    initial

--- src/main/java/org/example/student/StudentApp.java
+++ src/main/java/org/example/student/StudentApp.java
@@ -8,9 +8,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class StudentApp {
 
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(StudentConfig.class);
-       // ClassPathXmlApplicationContext context1 = new ClassPathXmlApplicationContext("StudentApp.xml");
-        Student student = context.getBean(Student.class);
+      //  ApplicationContext context = new AnnotationConfigApplicationContext(StudentConfig.class);
+        ClassPathXmlApplicationContext context1 = new ClassPathXmlApplicationContext("StudentApp.xml");
+        Student student = context1.getBean(Student.class);
         System.out.println("Subjects: " + student.getSubjects());
         System.out.println("Grades: " + student.getSubjectGrades());
     }
